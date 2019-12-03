c39b35e36847b3bdc4955ceff1ca6128e92e6268
Alexander Taylor <alexander@inclem.net>
2019-12-03 22:40:48 +0000

    Added various basic features for automatic camera handling

--- camera2/camera2.py
+++ camera2/camera2.py
@@ -1,6 +1,18 @@
 from kivy.event import EventDispatcher
+from kivy.graphics.texture import Texture
+from kivy.graphics import Fbo, Callback, Rectangle
+from kivy.properties import (BooleanProperty, StringProperty, ObjectProperty, OptionProperty, ListProperty)
+from kivy.clock import Clock
 
 from jnius import autoclass, cast, PythonJavaClass, java_method, JavaClass, MetaJavaClass, JavaMethod
+
+import logging
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 CameraManager = autoclass("android.hardware.camera2.CameraManager")
 PythonActivity = autoclass("org.kivy.android.PythonActivity")
@@ -30,6 +42,7 @@ CameraActions = autoclass("net.inclem.camera2.MyStateCallback$CameraActions")
 MyCaptureSessionCallback = autoclass("net.inclem.camera2.MyCaptureSessionCallback")
 CameraCaptureEvents = autoclass("net.inclem.camera2.MyCaptureSessionCallback$CameraCaptureEvents")
 
+_global_handler = Handler(Looper.getMainLooper())
 
 class Runnable(PythonJavaClass):
     __javainterfaces__ = ['java/lang/Runnable']
@@ -47,24 +60,241 @@ class Runnable(PythonJavaClass):
             traceback.print_exc()
 
 
-class PyCameraInterface(object):
+class PyCameraInterface(EventDispatcher):
     """
     Provides an API for querying details of the cameras available on Android.
     """
 
     camera_ids = []
 
-    camera_characteristics = {}
+    cameras = ListProperty()
+
+    java_camera_characteristics = {}
 
     java_camera_manager = ObjectProperty()
+
+    def __init__(self):
+        super().__init__()
+        self.java_camera_manager = cast("android.hardware.camera2.CameraManager",
+                                    context.getSystemService(Context.CAMERA_SERVICE))
+
+        self.camera_ids = self.java_camera_manager.getCameraIdList()
+        characteristics_dict = self.java_camera_characteristics
+        camera_manager = self.java_camera_manager
+        for camera_id in self.camera_ids:
+            characteristics_dict[camera_id] = camera_manager.getCameraCharacteristics(camera_id)
+
+            self.cameras.append(PyCameraDevice(
+                camera_id=camera_id,
+                java_camera_manager=camera_manager,
+                java_camera_characteristics=characteristics_dict[camera_id],
+            ))
+
+    def select_cameras(self, **conditions):
+        options = self.cameras
+        outputs = []
+        for camera in cameras:
+            for key, value in conditions.items():
+                if getattr(camera, key) != value:
+                    break
+            else:
+                outputs.append(camera)
+
+        return outputs
 
 class PyCameraDevice(EventDispatcher):
 
-    texture = ObjectProperty(None, allownone=True)
-    """
-    Texture holding the current drawing output of the camera, if any.
-    """
+    camera_id = StringProperty()
 
-    characteristics = ObjectProperty()
+    output_texture = ObjectProperty(None, allownone=True)
 
+    preview_active = BooleanProperty(False)
+    preview_texture = ObjectProperty(None, allownone=True)
+    preview_resolution = ListProperty()
+    preview_fbo = ObjectProperty(None, allownone=True)
+    java_preview_surface_texture = ObjectProperty(None)
+    java_preview_surface = ObjectProperty(None)
+    java_capture_request = ObjectProperty(None)
+    java_surface_list = ObjectProperty(None)
+    java_capture_session = ObjectProperty(None)
+
+    connected = BooleanProperty(False)
+
+    supported_resolutions = ListProperty()
+    # TODO: populate this
+
+    facing = OptionProperty("UNKNOWN", options=["UNKNOWN", "FRONT", "BACK", "EXTERNAL"])
+
+    java_camera_characteristics = ObjectProperty()
     java_camera_manager = ObjectProperty()
+    java_camera_device = ObjectProperty()
+    java_stream_configuration_map = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.register_event_type("on_opened")
+        self.register_event_type("on_closed")
+        self.register_event_type("on_disconnected")
+        self.register_event_type("on_error")
+
+        self._java_state_callback_runnable = Runnable(self._java_state_callback)
+        self._java_state_java_callback = MyStateCallback(self._java_state_callback_runnable)
+
+        self._java_capture_session_callback_runnable = Runnable(self._java_capture_session_callback)
+        self._java_capture_session_java_callback = MyCaptureSessionCallback(
+            self._java_capture_session_callback_runnable)
+
+        self._populate_camera_characteristics()
+
+    def on_opened(self, instance):
+        pass
+    def on_closed(self, instance):
+        pass
+    def on_disconnected(self, instance):
+        pass
+    def on_error(self, instance, error):
+        pass
+
+    def close(self):
+        self.java_camera_device.close()
+
+    def _populate_camera_characteristics(self):
+        self.java_stream_configuration_map = self.java_camera_characteristics.get(
+            CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+
+        self.supported_resolutions = [
+            (size.getWidth(), size.getHeight()) for size in
+            self.java_stream_configuration_map.getOutputSizes(SurfaceTexture(0).getClass())]
+
+        facing = self.java_camera_characteristics.get(
+            CameraCharacteristics.LENS_FACING)
+        if facing == CameraCharacteristics.LENS_FACING_BACK:
+            self.facing = "BACK"
+        elif facing == CameraCharacteristics.LENS_FACING_FRONT:
+            self.facing = "FRONT"
+        elif facing == CameraCharacteristics.LENS_FACING_EXTERNAL:
+            self.facing = "EXTERNAL"
+        else:
+            raise ValueError("Camera id {} LENS_FACING is unknown value {}".format(self.camera_id, facing))
+
+    def __str__(self):
+        return "<PyCameraDevice facing={}>".format(self.facing)
+    def __repr__(self):
+        return str(self)
+
+    def open(self):
+        self.java_camera_manager.openCamera(
+            self.camera_id,
+            self._java_state_java_callback,
+            _global_handler
+        )
+
+    def _java_state_callback(self, *args, **kwargs):
+        action = MyStateCallback.camera_action.toString()
+        camera_device = MyStateCallback.camera_device
+
+        self.java_camera_device = camera_device
+
+        logger.info("CALLBACK: camera event {}".format(action))
+        if action == "OPENED":
+            self.dispatch("on_opened", self)
+            self.connected = True
+        elif action == "DISCONNECTED":
+            self.dispatch("on_disconnected", self)
+            self.connected = False
+        elif action == "CLOSED":
+            self.dispatch("on_closed", self)
+            self.connected = False
+        elif action == "ERROR":
+            error = MyStateCallback.camera_error
+            self.dispatch("on_error", self, error)
+            self.connected = False
+        elif action == "UNKNOWN":
+            print("UNKNOWN camera state callback item")
+            self.connected = False
+        else:
+            raise ValueError("Received unknown camera action {}".format(action))
+
+    def start_preview(self, resolution):
+        if self.java_camera_device is None:
+            raise ValueError("Camera device not yet opened, cannot create preview stream")
+
+        if resolution not in self.supported_resolutions:
+            raise ValueError(
+                "Tried to open preview with resolution {}, not in supported resolutions {}".format(
+                    resolution, self.supported_resolutions))
+
+        if self.preview_active:
+            raise ValueError("Preview already active, can't start again without stopping first")
+
+        logger.info("Creating capture stream with resolution {}".format(resolution))
+
+        self.preview_resolution = resolution
+        self._prepare_preview_fbo(resolution)
+        self.preview_texture = Texture(
+            width=resolution[0], height=resolution[1], target=GL_TEXTURE_EXTERNAL_OES, colorfmt="rgba")
+        logger.info("Texture id is {}".format(self.preview_texture.id))
+        self.java_preview_surface_texture = SurfaceTexture(int(self.preview_texture.id))
+        self.java_preview_surface_texture.setDefaultBufferSize(*resolution)
+        self.java_preview_surface = Surface(self.java_preview_surface_texture)
+
+        self.java_capture_request = self.java_camera_device.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        self.java_capture_request.addTarget(self.java_preview_surface)
+        self.java_capture_request.set(
+            CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+        self.java_capture_request.set(
+            CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
+
+        self.java_surface_list = ArrayList()
+        self.java_surface_list.add(self.java_preview_surface)
+
+        self.java_camera_device.createCaptureSession(
+            self.java_surface_list,
+            self._java_capture_session_java_callback,
+            _global_handler,
+        )
+
+        return self.preview_fbo.texture
+
+    def _prepare_preview_fbo(self, resolution):
+        self.preview_fbo = Fbo(size=resolution)
+        self.preview_fbo['resolution'] = [float(f) for f in resolution]
+        self.preview_fbo.shader.fs = """
+            #extension GL_OES_EGL_image_external : require
+            #ifdef GL_ES
+                precision highp float;
+            #endif
+
+            /* Outputs from the vertex shader */
+            varying vec4 frag_color;
+            varying vec2 tex_coord0;
+
+            /* uniform texture samplers */
+            uniform sampler2D texture0;
+            uniform samplerExternalOES texture1;
+            uniform vec2 resolution;
+
+            void main()
+            {
+                gl_FragColor = texture2D(texture1, tex_coord0);
+            }
+        """
+        with self.preview_fbo:
+            Rectangle(size=resolution)
+
+    def _java_capture_session_callback(self, *args, **kwargs):
+        event = MyCaptureSessionCallback.camera_capture_event.toString()
+        logger.info("CALLBACK: capture event {}".format(event))
+
+        self.java_capture_session = MyCaptureSessionCallback.camera_capture_session
+
+        if event == "READY":
+            logger.info("Doing READY actions")
+            self.java_capture_session.setRepeatingRequest(self.java_capture_request.build(), None, None)
+            Clock.schedule_interval(self._update_preview, 0.)
+
+    def _update_preview(self, dt):
+        self.java_preview_surface_texture.updateTexImage()
+        self.preview_fbo.ask_update()
+        self.preview_fbo.draw()
+        self.output_texture = self.preview_fbo.texture
--- camera2/main.py
+++ camera2/main.py
@@ -3,286 +3,187 @@ from kivy.app import App
 from kivy import platform
 from kivy.lang import Builder
 from kivy.event import EventDispatcher
-from kivy.properties import (ObjectProperty, StringProperty, ListProperty)
+from kivy.properties import (ObjectProperty, StringProperty, ListProperty, BooleanProperty)
 from kivy.graphics.texture import Texture
 from kivy.graphics import Fbo, Callback, Rectangle
 from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.uix.stencilview import StencilView
+
+import time
+
+import logging
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 if platform == "android":
-    from jnius import autoclass, cast, PythonJavaClass, java_method, JavaClass, MetaJavaClass, JavaMethod
-    CameraManager = autoclass("android.hardware.camera2.CameraManager")
-    PythonActivity = autoclass("org.kivy.android.PythonActivity")
-    Context = autoclass("android.content.Context")
-    context = cast("android.content.Context", PythonActivity.mActivity)
+    from camera2 import PyCameraInterface
 
-    CameraDevice = autoclass("android.hardware.camera2.CameraDevice")
-    CaptureRequest = autoclass("android.hardware.camera2.CaptureRequest")
-    CameraCharacteristics = autoclass("android.hardware.camera2.CameraCharacteristics")
-
-    ArrayList = autoclass('java.util.ArrayList')
-    JavaArray = autoclass('java.lang.reflect.Array')
-
-    SurfaceTexture = autoclass('android.graphics.SurfaceTexture')
-    Surface = autoclass('android.view.Surface')
-    GL_TEXTURE_EXTERNAL_OES = autoclass(
-        'android.opengl.GLES11Ext').GL_TEXTURE_EXTERNAL_OES
-    ImageFormat = autoclass('android.graphics.ImageFormat')
-
-    Handler = autoclass("android.os.Handler")
-    Looper = autoclass("android.os.Looper")
-
-    MyStateCallback = autoclass("net.inclem.camera2.MyStateCallback")
-    CameraActions = autoclass("net.inclem.camera2.MyStateCallback$CameraActions")
-    # MyStateCallback = autoclass("org.kivy.android.MyStateCallback")
-
-    MyCaptureSessionCallback = autoclass("net.inclem.camera2.MyCaptureSessionCallback")
-    CameraCaptureEvents = autoclass("net.inclem.camera2.MyCaptureSessionCallback$CameraCaptureEvents")
-
-class Runnable(PythonJavaClass):
-    __javainterfaces__ = ['java/lang/Runnable']
-
-    def __init__(self, func):
-        super(Runnable, self).__init__()
-        self.func = func
-
-    @java_method('()V')
-    def run(self):
-        try:
-            self.func()
-        except:
-            import traceback
-            traceback.print_exc()
-
-
-# class StateCallback(JavaClass, metaclass=MetaJavaClass):
-#     __javaclass__ = "android/hardware/camera2/CameraDevice.StateCallback"
-#     # __metaclass__ = MetaJavaClass  # py2
-
-
-#     def __init__(self, callback):
-#         super(StateCallback, self).__init__()
-#         self._callback = callback
-
-#     @java_method("(Landroid/hardware/camera2/CameraDevice;)V")
-#     def onClosed(self, camera_device):
-#         print("Closed {}".format(camera_device))
-#         self.camera_device = None
-
-#     @java_method("(Landroid/hardware/camera2/CameraDevice;)V")
-#     def onDisconnected(self, camera_device):
-#         print("Disconnected {}".format(camera_device))
-#         pass
-
-#     @java_method("(Landroid/hardware/camera2/CameraDevice;I)V")
-#     def onOpened(self, camera_device, error_code):
-#         print("Opened {}".format(camera_device))
-#         self.camera_device = camera_device
-#         self._callback(self.camera_device)
-
-#     @java_method("(Landroid/hardware/camera2/CameraDevice;)V")
-#     def onError(self, camera_device):
-#         print("Error {}".format(camera_device))
-#         pass
-
-
-class AndroidCamera2(EventDispatcher):
-    camera_device = ObjectProperty(None, allownone=True)
-
-    preview_surface_texture = ObjectProperty(None, allownone=True)
-    preview_surface = ObjectProperty(None, allownone=True)
-    preview_texture = ObjectProperty(None, allownone=True)
-
+class CameraDisplayWidget(StencilView):
     texture = ObjectProperty(None, allownone=True)
 
-    def __init__(self):
-        super().__init__()
-        self.camera_device = None
+    resolution = ListProperty([1, 1])
 
-        self.camera_manager = cast("android.hardware.camera2.CameraManager",
-                                    context.getSystemService(Context.CAMERA_SERVICE))
+    tex_coords = ListProperty([0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0])
+    correct_camera = BooleanProperty(False)
 
-        self.handler = Handler(Looper.getMainLooper())
+    _rect_pos = ListProperty([0, 0])
+    _rect_size = ListProperty([1, 1])
 
-        self.state_runnable = Runnable(self.state_callback)
-        self.state_callback = MyStateCallback(self.state_runnable)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-        self.capture_session_runnable = Runnable(self.capture_session_callback_method)
-        self.capture_session_callback = MyCaptureSessionCallback(self.capture_session_runnable)
+        self.bind(
+            pos=self._update_rect,
+            size=self._update_rect,
+            resolution=self._update_rect,
+        )
 
-        self.resolution = (1920, 1080)
-
-        width, height = self.resolution
-        self.preview_texture = Texture(
-            width=width, height=height, target=GL_TEXTURE_EXTERNAL_OES, colorfmt="rgba")
-
-        self.prepare_texture()
-
-    def prepare_texture(self):
-        self.fbo = Fbo(size=self.resolution)
-        self.fbo['resolution'] = [float(f) for f in self.resolution]
-        self.fbo.shader.fs = """
-            #extension GL_OES_EGL_image_external : require
-            #ifdef GL_ES
-                precision highp float;
-            #endif
-
-            /* Outputs from the vertex shader */
-            varying vec4 frag_color;
-            varying vec2 tex_coord0;
-
-            /* uniform texture samplers */
-            uniform sampler2D texture0;
-            uniform samplerExternalOES texture1;
-            uniform vec2 resolution;
-
-            void main()
-            {
-                vec2 coord = vec2(
-                    tex_coord0.y * (resolution.y / resolution.x),
-                    1. -tex_coord0.x
-                    );
-                gl_FragColor = texture2D(texture1, tex_coord0);
-            }
-        """
-        with self.fbo:
-            Rectangle(size=self.resolution)
-
-    def update(self, dt):
-        self.preview_surface_texture.updateTexImage()
-        self.fbo.ask_update()
-        self.fbo.draw()
-        self.texture = self.fbo.texture
-
-    def on_texture(self, *args):
-        App.get_running_app().texture = self.texture
-
-    def state_callback(self, *args, **kwargs):
-        # print("test callback called with args {} kwargs {}".format(args, kwargs))
-        # print("MyStateCallback.camera_device is {}".format(MyStateCallback.camera_device))
-        # print("action is", MyStateCallback.camera_action)
-        # print("error is", MyStateCallback.camera_error)
-
-        action = MyStateCallback.camera_action.toString()
-        camera_device = MyStateCallback.camera_device
-        self.camera_device = camera_device
-        if action == "OPENED":
-            self.callback_camera_opened(camera_device)
-        elif action == "DISCONNECTED":
-            self.callback_camera_disconnected(camera_device)
-        elif action == "CLOSED":
-            self.callback_camera_closed(camera_device)
-        elif action == "ERROR":
-            error = MyStateCallback.camera_error
-            self.callback_camera_error(camera_device, error)
-        elif action == "UNKNOWN":
-            self.callback_camera_opened(camera_device)
+    def on_correct_camera(self, instance, correct):
+        print("Correct became", correct)
+        if correct:
+            self.tex_coords = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0]
+            print("Set 0!")
         else:
-            print('COULD NOT RECOGNISE CAMERA_ACTION', action)
+            self.tex_coords = [0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0]
+            print("Set 1!")
 
-    def capture_session_callback_method(self, *args, **kwargs):
-        print("MyCaptureSessionCallback: event is {}".format(
-            MyCaptureSessionCallback.camera_capture_event.toString()))
+    def on_tex_coords(self, instance, value):
+        print("tex_coords became", self.tex_coords)
 
-        if MyCaptureSessionCallback.camera_capture_event.toString() == "READY":
-            print("Starting update")
+    def _update_rect(self, *args):
+        self._update_rect_to_fill()
 
-            self.the_capture_session = MyCaptureSessionCallback.camera_capture_session
-            self.the_capture_session.setRepeatingRequest(self.capture_request.build(), None, None)
+    def _update_rect_to_fit(self, *args):
+        w, h = self.resolution
+        aspect_ratio = h / w
 
-            Clock.schedule_interval(self.update, 1./30.)
+        aspect_width = self.width
+        aspect_height = self.width * h / w
+        if aspect_height > self.height:
+            aspect_height = self.height
+            aspect_width = aspect_height * w / h
 
-    def callback_camera_opened(self, camera_device):
-        print('Opened callback', camera_device)
+        aspect_height = int(aspect_height)
+        aspect_width = int(aspect_width)
 
-    def callback_camera_closed(self, camera_device):
-        print('Closed callback', camera_device)
-        self.camera_device = None
+        self._rect_pos = [self.center_x - aspect_width / 2,
+                          self.center_y - aspect_height / 2]
 
-    def callback_camera_error(self, camera_device, error):
-        print('Error callback', camera_device, error)
-        self.camera_device = None
+        self._rect_size = [aspect_width, aspect_height]
 
-    def callback_camera_disconnected(self, camera_device):
-        print('Disconnected callback', camera_device)
-        self.camera_device = None
+    def _update_rect_to_fill(self, *args):
+        w, h = self.resolution
 
-    def camera_ids(self):
-        return self.camera_manager.getCameraIdList()
+        aspect_ratio = h / w
 
-    def get_characteristics(self, camera_id):
-        characteristics = self.camera_manager.getCameraCharacteristics(camera_id)
-        return characteristics
+        aspect_width = self.width
+        aspect_height = self.width * h / w
+        if aspect_height < self.height:
+            aspect_height = self.height
+            aspect_width = aspect_height * w / h
 
-    def open_camera(self, index):
-        camera_id = self.camera_ids()[index]
+        aspect_height = int(aspect_height)
+        aspect_width = int(aspect_width)
 
-        self.characteristics = self.get_characteristics(camera_id)
+        self._rect_pos = [self.center_x - aspect_width / 2,
+                          self.center_y - aspect_height / 2]
 
-        self.camera_manager.openCamera(camera_id, self.state_callback, self.handler)
-
-    def create_preview_stream(self):
-        if self.camera_device is None:
-            raise ValueError("Cannot open preview without a camera opened")
-
-        if self.preview_surface_texture is not None:
-            raise ValueError("Cannot open preview, a preview surface already exists")
-
-
-        self.preview_surface_texture = SurfaceTexture(int(self.preview_texture.id))
-        self.preview_surface_texture.setDefaultBufferSize(*self.resolution)
-
-        self.stream_configuration_map = self.characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-
-        print("output sizes are",
-              [(size.getWidth(), size.getHeight()) for size in self.stream_configuration_map.getOutputSizes(self.preview_surface_texture.getClass())])
-
-        self.preview_surface = Surface(self.preview_surface_texture)
-
-        self.capture_request = self.camera_device.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-        self.capture_request.addTarget(self.preview_surface)
-        self.capture_request.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
-        self.capture_request.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
-
-        self.surface_list = ArrayList()
-        self.surface_list.add(self.preview_surface)
-        self.camera_device.createCaptureSession(
-            self.surface_list,
-            self.capture_session_callback,
-            self.handler)
-
-
-
+        self._rect_size = [aspect_width, aspect_height]
 
 class CameraApp(App):
     texture = ObjectProperty(None, allownone=True)
+    camera_resolution = ListProperty([1920, 1080])
+
+    current_camera = ObjectProperty(None, allownone=True)
+
+    cameras_to_use = ListProperty()
+
     def build(self):
-        if platform == "android":
-            root = Builder.load_file("androidcamera.kv")
+        root = Builder.load_file("androidcamera.kv")
 
-            self.camera_interface = AndroidCamera2()
+        self.camera_interface = PyCameraInterface()
 
-            Clock.schedule_interval(self.update, 0)
+        Clock.schedule_interval(self.update, 0)
 
-            return root
+        self.debug_print_camera_info()
 
-    def print_camera_info(self):
-        camera_ids = self.camera_interface.camera_ids()
-        print("ids are", camera_ids, type(camera_ids))
+        self.inspect_cameras()
 
-        for camera_id in camera_ids:
-            characteristics = self.camera_interface.get_characteristics(camera_id)
-            print("Camera id {} characteristics are {}".format(camera_id, characteristics.getKeys()))
+        self.restart_stream()
 
-    def open_camera(self, index):
-        self.camera_interface.open_camera(index)
+        return root
 
-    def create_preview_stream(self):
-        if self.camera_interface.camera_device is None:
-            self.camera_interface.open_camera(0)
-        self.camera_interface.create_preview_stream()
+    def inspect_cameras(self):
+        cameras = self.camera_interface.cameras
+
+        for camera in cameras:
+            if camera.facing == "BACK":
+                self.cameras_to_use.append(camera)
+        for camera in cameras:
+            if camera.facing == "FRONT":
+                self.cameras_to_use.append(camera)
+
+    def rotate_cameras(self):
+        self.ensure_camera_closed()
+        self.cameras_to_use = self.cameras_to_use[1:] + [self.cameras_to_use[0]]
+        self.stream_camera(self.cameras_to_use[0])
+
+    def restart_stream(self):
+        self.ensure_camera_closed()
+        Clock.schedule_once(self._restart_stream, 0)
+
+    def _restart_stream(self, dt):
+        self.stream_camera(self.cameras_to_use[0])
+
+    def debug_print_camera_info(self):
+        cameras = self.camera_interface.cameras
+        camera_infos = ["Camera ID {}, facing {}".format(c.camera_id, c.facing) for c in cameras]
+        for camera in cameras:
+            print("Camera ID {}, facing {}, resolutions {}".format(
+                camera.camera_id, camera.facing, camera.supported_resolutions))
+
+    def stream_camera_index(self, index):
+        self.stream_camera(self.camera_interface.cameras[index])
+
+    def stream_camera(self, camera):
+        resolution = (1920, 1080)
+        self.camera_resolution = resolution
+        camera.open()
+        time.sleep(0.5)
+        if camera.facing == "FRONT":
+            self.root.ids.cdw.correct_camera = True
+        else:
+            self.root.ids.cdw.correct_camera = False
+        self.texture = camera.start_preview(resolution)
+
+        self.current_camera = camera
+
+    def on_texture(self, instance, value):
+        print("App texture changed to {}".format(value))
 
     def update(self, dt):
         self.root.canvas.ask_update()
+
+    def ensure_camera_closed(self):
+        if self.current_camera is not None:
+            self.current_camera.close()
+            self.current_camera = None
+
+    def on_pause(self):
+
+        logger.info("Closing camera due to pause")
+        self.ensure_camera_closed()
+
+        return super().on_pause()
+
+    def on_resume(self):
+        logger.info("Opening camera due to resume")
+        self.restart_stream()
 
 
 if __name__ == "__main__":
